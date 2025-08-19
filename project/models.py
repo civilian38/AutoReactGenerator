7064a05432c608a93e9c7939aac8b1221d77f88a
civilian38 <70161936+civilian38@users.noreply.github.com>
2025-08-19 10:28:53 +0000

    feat/implemented project model

--- project/models.py
+++ project/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from authentication.models import ARUser
 
-# Create your models here.
+class Project(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField(null=True, blank=True)
+    created_by = models.ForeignKey(ARUser, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        owner_name = self.created_by.nickname if self.created_by.nickname else self.created_by.username
+        return f'{owner_name} - {self.name}'
