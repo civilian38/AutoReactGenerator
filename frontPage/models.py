25408c3e452fd0d34f8211d28ffc7bcab7f0f484
civilian38 <gyk23953@gmail.com>
2025-09-20 22:39:09 +0900

    feat/initializing front page

---
+++ frontPage/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from project.models import Project
+
+class FrontPage(models.Model):
+    project_under = models.ForeignKey(Project, on_delete=models.CASCADE)
+    url = models.URLField()
+    page_description = models.TextField()
+    is_implemented = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.project_under.name} - {self.url}'
