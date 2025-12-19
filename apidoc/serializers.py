46c7dd1a173764c73f4e1b89ba1ce5880979ad35
civilian38 <gyk23953@gmail.com>
2025-12-19 19:18:32 +0900

    fix/url validation logic

--- apidoc/serializers.py
+++ apidoc/serializers.py
@@ -23,9 +23,6 @@ class APIDocCreateSerializer(serializers.ModelSerializer):
     def validate(self, attrs):
         incoming_url = attrs.get('url', '')
 
-        if incoming_url.startswith(('http://', 'https://')):
-            return attrs
-
         view = self.context.get('view')
         project_id = view.kwargs.get('project_id')
 
@@ -36,10 +33,14 @@ class APIDocCreateSerializer(serializers.ModelSerializer):
             except Project.DoesNotExist:
                 raise ValidationError('Project does not exist')
 
-        if project and project.base_url:
-            base = project.base_url.rstrip('/')
+        if project and project.base_api_url:
+            if incoming_url.startswith(project.base_api_url):
+                attrs['url'] = incoming_url.rstrip('/') + '/'
+                return attrs
+
+            base = project.base_api_url.rstrip('/')
             path = incoming_url.strip('/')
-            full_url = f"{base}/{path}"
+            full_url = f"{base}/{path}/"
 
             # 유효성 재검사
             validator = URLValidator()
@@ -51,6 +52,14 @@ class APIDocCreateSerializer(serializers.ModelSerializer):
         return attrs
 
 class APIDocListSerializer(serializers.ModelSerializer):
+    url = serializers.SerializerMethodField()
+
     class Meta:
         model = APIDoc
         fields = ['id', 'url', 'http_method']
+
+    def get_url(self, obj):
+        project = obj.project_under
+        if project and project.base_api_url:
+            return obj.url.removeprefix(project.base_api_url.rstrip('/'))
+        return "Unknown Error"
--- project/serializers.py
+++ project/serializers.py
@@ -3,19 +3,29 @@ from rest_framework import serializers
 from .models import Project
 from authentication.serializers import ARUserInfoSerializer
 
-class ProjectLCSerializer(serializers.ModelSerializer):
+class ProjectListSerializer(serializers.ModelSerializer):
     created_by = serializers.PrimaryKeyRelatedField(read_only=True)
 
-    def validate_base_url(self, value):
+    class Meta:
+        model = Project
+        fields = ['id', 'name', 'base_web_url', 'description', 'created_by', 'created_at']
+
+class ProjectCreateSerializer(serializers.ModelSerializer):
+    created_by = serializers.PrimaryKeyRelatedField(read_only=True)
+
+    def validate_base_web_url(self, value):
         return value.rstrip('/')
+
+    def validate_base_api_url(self, value):
+        return value.rstrip('/') + '/'
     
     class Meta:
         model = Project
-        fields = ['id', 'name', 'base_url', 'description', 'created_by', 'created_at']
+        fields = '__all__'
 
 class ProjectRetrieveSerializer(serializers.ModelSerializer):
     created_by = ARUserInfoSerializer(read_only=True)
 
     class Meta:
         model = Project
-        fields = ['id', 'name', 'created_by', 'base_url', 'description', 'created_at']
+        fields = '__all__'
