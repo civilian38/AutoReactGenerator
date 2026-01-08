bb06026c1529e63aa944624e15a056f5c4fe8e59
LEE Kyuwon <70161936+civilian38@users.noreply.github.com>
2026-01-08 11:31:55 +0000

    feat/session status change

--- codeGenerate/urls.py
+++ codeGenerate/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import GenerationSessionLCView
+from .views import GenerationSessionLCView, SessionStatusCompletedView, SessionStatusDiscardedView
 
 urlpatterns = [
     path('sessions/<int:project_id>/', GenerationSessionLCView.as_view(), name='session-list-create'),
+    path('session/<int:pk>/complete/', SessionStatusCompletedView.as_view(), name='session-status-complete'),
+    path('session/<int:pk>/discarded/', SessionStatusDiscardedView.as_view(), name='session-status-discarded')
 ]
--- codeGenerate/views.py
+++ codeGenerate/views.py
@@ -1,4 +1,11 @@
+from django.shortcuts import get_object_or_404
+from django.db.models import F, Q
+from django.db import transaction
+
 from rest_framework.generics import ListCreateAPIView
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
 from AutoReactGenerator.permissions import SubClassIsOwnerOrReadOnly
 
@@ -23,3 +30,53 @@ class GenerationSessionLCView(ListCreateAPIView):
         project = Project.objects.get(id=self.kwargs.get('project_id'))
         serializer.save(project_under=project)
 
+class SessionStatusCompletedView(APIView):
+    def post(self, request, pk):
+        session_object = get_object_or_404(GenerationSession, pk=pk)
+        self.check_object_permissions(request, session_object)
+
+        with transaction.atomic():
+            target_files = session_object.related_files.exclude(
+                Q(draft_content__isnull=True) | Q(draft_content='')
+            )
+
+            updated_count = target_files.update(
+                content=F('draft_content'),
+                draft_content=None  
+            )
+
+            session_object.status = "COMPLETED"
+            session_object.save()
+
+        return Response(
+            {
+                "message": "Session completed and files updated.", 
+                "updated_files_count": updated_count
+            }, 
+            status=status.HTTP_200_OK
+        )
+
+class SessionStatusDiscardedView(APIView):
+    def post(self, request, pk):
+        session_object = get_object_or_404(GenerationSession, pk=pk)
+        self.check_object_permissions(request, session_object)
+
+        with transaction.atomic():
+            target_files = session_object.related_files.exclude(
+                Q(draft_content__isnull=True) | Q(draft_content='')
+            )
+
+            draft_count = target_files.update(
+                draft_content=None  
+            )
+
+            session_object.status = "DISCARDED"
+            session_object.save()
+
+        return Response(
+            {
+                "message": "Session completed and draft contents discarded.", 
+                "updated_files_count": draft_count
+            }, 
+            status=status.HTTP_200_OK
+        )
