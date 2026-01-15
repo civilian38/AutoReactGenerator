6a32437e4ed8abe81ab453b28db540a1d52dd005
LEE Kyuwon <70161936+civilian38@users.noreply.github.com>
2026-01-15 10:12:08 +0000

    fix/api doc admin page

--- apidoc/admin.py
+++ apidoc/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from .models import APIDoc
 
-admin.site.register(APIDoc)
+@admin.register(APIDoc)
+class APIDocAdmin(admin.ModelAdmin):
+    fields = ('id', 'url', 'http_method', 'request_format', 'request_headers', 'query_params', 'response_format', 'description', 'created_at', 'created_by', 'project_under')
+    readonly_fields = ('id', 'created_at',)
