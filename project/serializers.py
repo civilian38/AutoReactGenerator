1fdc76c038123ec37f6c83511334059c374ddbcc
LEE Kyuwon <70161936+civilian38@users.noreply.github.com>
2026-01-16 09:16:50 +0000

    feat/added project handover context

--- project/serializers.py
+++ project/serializers.py
@@ -22,6 +22,7 @@ class ProjectCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Project
         fields = '__all__'
+        read_only_fields = ('handover_context',)
 
 class ProjectRetrieveSerializer(serializers.ModelSerializer):
     created_by = ARUserInfoSerializer(read_only=True)
