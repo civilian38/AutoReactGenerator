fa2b312e46ae24ca6a04b2ee4c760378b21e8ff0
LEE Kyuwon <70161936+civilian38@users.noreply.github.com>
2026-02-16 08:00:11 +0000

    parameter CRUD

--- apidoc/permissions.py
+++ apidoc/permissions.py
@@ -1,4 +1,5 @@
 from rest_framework import permissions
+from .models import APIDoc
 
 class IsBodyOwner(permissions.BasePermission):
     def has_permission(self, request, view):
@@ -10,4 +11,15 @@ class IsBodyOwner(permissions.BasePermission):
         if request.user and request.user.is_authenticated:
             return obj.apidoc.project_under.created_by == request.user
         return False
+
+class IsParameterOwner(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+           return True
+        return request.user and request.user.is_authenticated
     
+    def has_object_permission(self, request, view, obj):
+        if request.user and request.user.is_authenticated:
+            project = obj.project_under
+            return project.created_by == request.user
+        return False
--- apidoc/serializers.py
+++ apidoc/serializers.py
@@ -3,7 +3,7 @@ from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
 
 from project.models import Project
-from .models import APIDoc, APIRequestBody, APIResponseBody
+from .models import APIDoc, APIRequestBody, APIResponseBody, URLParameter
 
 class APIRequestBodySerializer(serializers.ModelSerializer):
     apidoc = serializers.PrimaryKeyRelatedField(read_only=True)
@@ -38,12 +38,18 @@ class APIResponseListSerializer(serializers.ModelSerializer):
     def get_description(self, obj):
         description = obj.description
         return description[:30]
+    
+class URLParameterListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = URLParameter
+        fields = ('id', 'parameter', 'description')
 
 class APIDocSerializer(serializers.ModelSerializer):
     created_by = serializers.PrimaryKeyRelatedField(read_only=True)
     project_under = serializers.PrimaryKeyRelatedField(read_only=True)
     request_bodies = APIRequestListSerializer(many=True, read_only=True)
     response_bodies = APIResponseListSerializer(many=True, read_only=True)
+    url_parameters = URLParameterListSerializer(many=True)
 
     class Meta:
         model = APIDoc
@@ -102,3 +108,24 @@ class APIDocListSerializer(serializers.ModelSerializer):
         if project and project.base_api_url:
             return obj.url.removeprefix(project.base_api_url.rstrip('/'))
         return "Unknown Error"
+
+class URLParameterCreateSerializer(serializers.ModelSerializer):
+    project_under = serializers.PrimaryKeyRelatedField(read_only=True)
+
+    class Meta:
+        model = URLParameter
+        fields = ('id', 'project_under', 'parameter', 'description')
+
+class URLParameterRetrieveUpdateSerializer(serializers.ModelSerializer):
+    apidocs = APIDocListSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = URLParameter
+        fields = ('parameter', 'description', 'apidocs')
+
+class ParameterRelationUpdateSerializer(serializers.Serializer):
+    to_add = serializers.ListField(child=serializers.IntegerField(), required=False, default=[])
+    to_pop = serializers.ListField(child=serializers.IntegerField(), required=False, default=[])
+
+    class Meta:
+        fields = ('to_add', 'to_pop')
